cacccfec6c16c2f302e0656053b8ee30955fc965
Guilherme Peres Mundim <46054442+Guilherme0202PM@users.noreply.github.com>
2025-08-08 16:53:22 -0300

    Desafio 2

---
+++ Desafio 2/desafio.js
@@ -0,0 +1,21 @@
+ alert('Boas-vindas ao nosso site')
+
+let idade = 25;
+
+let numeroVendas = 50;
+
+let saldoDisponivel = 1000;
+
+alert("Erro! Preencha todos os campos.");
+
+let mensagemErro = "Erro! Preencha todos os campos.";
+
+alert(mensagemErro);
+
+let nomeUsuario = prompt('Digite seu Nome');
+
+let idadeUsuario = prompt('Digite sua idade');
+
+if (idadeUsuario >= 18) {
+    alert('Já pode tirar habilitação ' + nomeUsuario);
+}
