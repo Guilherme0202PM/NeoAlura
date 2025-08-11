eac8fea0dc21e4d8e52ae493bf1a1712cb973477
Guilherme Peres Mundim <46054442+Guilherme0202PM@users.noreply.github.com>
2025-08-11 18:45:27 -0300

    Desafio 5

---
+++ Desafio 5/app.js
@@ -0,0 +1,34 @@
+//Desafio 1
+let i = 1;
+while (i <= 10) {
+    alert('temporada ' + i); 
+    console.log(i);
+    i++;
+}
+
+//Desafio 2
+i = 10;
+while (i >= 0) {
+    alert('temporada ' + i); 
+    console.log(i);
+    i--;
+}
+
+//Desafio 3
+let numero = prompt('Digite um numero');
+
+while (numero >= 0) {
+    alert('temporada ' + numero); 
+    console.log(numero);
+    numero--;
+}
+
+//Desafio 4
+numero = prompt('Digite um numero');
+i = 0;
+
+while (i <= numero) {
+    alert('temporada ' + i); 
+    console.log(i);
+    i++;
+}
